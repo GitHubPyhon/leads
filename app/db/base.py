71cdfb7938c021fcd0a574568719ac5862c43bf6
roman <romank@enapps.co.uk>
2023-05-03 00:12:24 +0300

    fix database url

--- app/db/base.py
+++ app/db/base.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://roman:superpwd@postgres/leads"
-SQLALCHEMY_DATABASE_URL = "postgresql://james:james@localhost/leads"
+SQLALCHEMY_DATABASE_URL = "postgresql://roman:superpwd@postgres/leads"
+# SQLALCHEMY_DATABASE_URL = "postgresql://james:james@localhost/leads"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
